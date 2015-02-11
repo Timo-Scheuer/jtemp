42cd6c2ea32be6be7df4a7020a936c5bc05ba4de
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2015-02-11 08:28:00 +0000

    fixes to m2e plugin. now appears to work coreectly

--- m2e-jtemp/uk.ac.soton.ecs.jsh2.m2e.jtemp/src/main/java/uk/ac/soton/ecs/jsh2/m2e/jtemp/JtempPluginConfigurator.java
+++ m2e-jtemp/uk.ac.soton.ecs.jsh2.m2e.jtemp/src/main/java/uk/ac/soton/ecs/jsh2/m2e/jtemp/JtempPluginConfigurator.java
@@ -1,12 +1,8 @@
 package uk.ac.soton.ecs.jsh2.m2e.jtemp;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URI;
 
 import org.apache.maven.plugin.MojoExecution;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IPath;
@@ -14,13 +10,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
-import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.m2e.jdt.AbstractSourcesGenerationProjectConfigurator;
 import org.eclipse.m2e.jdt.IClasspathDescriptor;
 import org.eclipse.m2e.jdt.IJavaProjectConfigurator;
 
@@ -28,9 +23,12 @@ import org.eclipse.m2e.jdt.IJavaProjectConfigurator;
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
  *
  */
-public class JtempPluginConfigurator extends AbstractProjectConfigurator implements IJavaProjectConfigurator {
+public class JtempPluginConfigurator extends AbstractSourcesGenerationProjectConfigurator
+		implements
+IJavaProjectConfigurator
+{
 
-	private static final String BUNDLE_ID = "uk.ac.soton.ecs.jsh2.m2e.jtemp";
+	static final String BUNDLE_ID = "uk.ac.soton.ecs.jsh2.m2e.jtemp";
 
 	@Override
 	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
@@ -38,56 +36,39 @@ public class JtempPluginConfigurator extends AbstractProjectConfigurator impleme
 	}
 
 	@Override
-	public void configureRawClasspath(ProjectConfigurationRequest request, IClasspathDescriptor classpath,
+	protected File[] getSourceFolders(ProjectConfigurationRequest request, MojoExecution mojoExecution,
 			IProgressMonitor monitor) throws CoreException
 	{
-		final ILog log = Platform.getLog(Platform.getBundle(BUNDLE_ID));
-
-		final IProject project = request.getProject();
-		final IJavaProject javaProject = JavaCore.create(project);
-		final IMavenProjectFacade facade = request.getMavenProjectFacade();
-
-		final File base = facade.getMavenProject(monitor).getBasedir();
-		try {
-			createSource(classpath, project, javaProject, facade, base + "/target/generated-sources/jtemp");
-		} catch (final Throwable e) {
-			log.log(new Status(IStatus.ERROR, BUNDLE_ID, "Error adding generated classes to the build path", e));
-		}
-		try {
-			createSource(classpath, project, javaProject, facade, base + "/target/generated-test-sources");
-		} catch (final Throwable e) {
-			log.log(new Status(IStatus.ERROR, BUNDLE_ID, "Error adding generated test classes to the build path", e));
-		}
-	}
-
-	protected void createSource(IClasspathDescriptor classpath, final IProject project, final IJavaProject javaProject,
-			final IMavenProjectFacade facade, String sourceAttribute) throws IOException, CoreException
-	{
-		if (!new File(sourceAttribute).isAbsolute()) {
-			sourceAttribute = project.getLocationURI().toString().replace("file:/", "") + "/" + sourceAttribute;
-		}
-		final File file = new File(sourceAttribute);
-		final File canonicalFile = file.getCanonicalFile();
-		if (!canonicalFile.exists())
-			canonicalFile.mkdirs();
-		URI uri = canonicalFile.toURI();
-		uri = project.getPathVariableManager().convertToRelative(uri, false, "TEST");
-
-		final String relative = uri.toString().replace("PROJECT_LOC", "");
-		final IFolder folder = project.getFolder(relative);
-
-		IPath path = folder.getProjectRelativePath();
-		path = javaProject.getPath().append(path);
-
-		if (!classpath.containsPath(path))
-			classpath.addSourceEntry(path, facade.getOutputLocation(), true);
+		return new File[] {
+				new File("/target/generated-sources/jtemp"),
+				new File("/target/generated-test-sources/jtemp")
+		};
 	}
 
 	@Override
-	public void configureClasspath(IMavenProjectFacade facade, IClasspathDescriptor classpath, IProgressMonitor monitor)
-			throws CoreException
+	public void configureRawClasspath(ProjectConfigurationRequest request, IClasspathDescriptor classpath,
+			IProgressMonitor monitor) throws CoreException
 	{
-		// All Done in getRawClassPath
+		final IMavenProjectFacade facade = request.getMavenProjectFacade();
+
+		assertHasNature(request.getProject(), JavaCore.NATURE_ID);
+
+		final File base = facade.getProject().getLocation().toFile();
+		for (final MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
+			final File[] sources = getSourceFolders(request, mojoExecution, monitor);
+
+			for (File source : sources) {
+				source = new File(base, source.toString());
+				final IPath sourcePath = getFullPath(facade, source);
+
+				final ILog log = Platform.getLog(Platform.getBundle(JtempPluginConfigurator.BUNDLE_ID));
+				log.log(new Status(IStatus.INFO, BUNDLE_ID, source + " " + sourcePath));
+
+				if (sourcePath != null) {
+					classpath.addSourceEntry(sourcePath, facade.getOutputLocation(), true);
+				}
+			}
+		}
 	}
 
 	@Override
